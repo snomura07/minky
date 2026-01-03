111c7dc74619749fbc66388be0f8965f62cc11f3
snomura <you@example.com>
2026-01-03 21:02:14 +0900

    update init mysql

---
+++ src/batch/insert_dummy_records.sql
@@ -0,0 +1,11 @@
+INSERT INTO migrations (name, rev) VALUES
+('init_schema', 1),
+('add_users_table', 1),
+('add_products_table', 1),
+('modify_users_add_status', 2),
+('create_orders_table', 1),
+('add_order_items_table', 1),
+('update_products_price_type', 2),
+('add_logs_table', 1),
+('modify_orders_add_tracking', 2),
+('cleanup_unused_tables', 3);
