2cb978e9d3268f23ce5daf3f3fd3f573ee3f4bd9
snomura <you@example.com>
2026-01-17 21:08:59 +0900

    add migration

--- src/database/migrations/2025_01_01_000002_add.php
+++ src/database/migrations/2025_01_01_000002_add.php
@@ -8,9 +8,22 @@ return new class extends Migration
 {
     public function up(): void
     {
+        Capsule::schema()->create('weather_reports', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->float('latitude')->comment('緯度');
+            $table->float('longitude')->comment('経度');
+            $table->string('measured_time')->comment('観測時刻');
+            $table->float('temperature')->comment('気温[℃]');
+            $table->float('humidity')->comment('湿度[%]');
+            $table->float('wind_speed')->comment('風速[m/s]');
+            $table->float('precipitation')->comment('降水量[mm]');
+            $table->timestamps();
+        });
+
     }
 
     public function down(): void
     {
+        Capsule::schema()->dropIfExists('weather_reports');
     }
 };
