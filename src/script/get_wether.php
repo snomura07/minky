75a3a53f688844cd7ac734168acc05c4a0c6b1f2
snomura <you@example.com>
2026-01-24 16:34:18 +0900

    update

--- src/script/get_wether.php
+++ src/script/get_wether.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/../bootstrap/app.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/actions/weather_report.php';
 
-$lat = 35.6895;  // Fukui
-$lon = 139.6917; // Fukui
+$lat = 36.063;  // Fukui
+$lon = 136.218; // Fukui
 
 $weatherReportAction = new WeatherReportAction();
 $weatherReport = $weatherReportAction->fetchAndStore($lat, $lon);
