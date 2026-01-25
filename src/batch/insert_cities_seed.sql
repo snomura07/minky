731e30f8fa355dfec3fa48f3780ba399c05fd32e
snomura <you@example.com>
2026-01-25 21:10:22 +0900

    add batchs

---
+++ src/batch/insert_cities_seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO cities (
+    prefecture_name,
+    city_name,
+    latitude,
+    longitude,
+    created_at,
+    updated_at
+) VALUES
+    ('Tokyo', 'Shinjuku', 35.69, 139.69, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('Fukui', 'Fukui', 36.06, 136.22, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+;
---
+++ src/batch/update_city_id.sql
@@ -0,0 +1,7 @@
+UPDATE weather_reports
+SET city_id = 1
+WHERE latitude = 35.69;
+
+UPDATE weather_reports
+SET city_id = 2
+WHERE latitude = 36.06;
